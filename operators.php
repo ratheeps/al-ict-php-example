8219f0887a1e478a9f7875654b070c31f1bf871d
Ratheep <jayakkumarratheepan@adventus.io>
2020-04-30 21:33:55 +0530

    init

---
+++ operators.php
@@ -0,0 +1,39 @@
+<?php
+
+$num1 = 10;
+$num2 = 20;
+$num7 = 3;
+
+$num3 = $num1 + $num2;
+echo $num3;
+
+echo "<br/>";
+
+$num4 = $num2 - $num1;
+echo $num4;
+
+echo "<br/>";
+
+$num5 = $num2 * $num1;
+echo $num5;
+
+
+echo "<br/>";
+
+$num6 = $num2 / $num1;
+echo $num6;
+
+echo "<br/>";
+
+$num8 = $num1 % $num7;
+echo $num8;
+
+echo "<br/>";
+
+$num9 = $num1 ** $num7;
+echo $num9;
+
+echo "<br/>";
+
+$num1 -= $num2; // $num1 = $num1 + $num2
+echo $num1;
